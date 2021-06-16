0024b7be687d841d79f73ed7235fe4f421be3660
shivamchandra75 <chandrshivam@gmail.com>
2021-06-16 19:56:17 +0530

    pop semiFinal work

--- app.js
+++ app.js
@@ -1,64 +1,39 @@
 // Selectors
-const main = document.querySelector("#main");
-const button = document.querySelector(".btn");
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-button]");
+const overlay = document.querySelector("#overlay");
 
-// Event Lisners
-button.addEventListener("click", addCard);
+// Eventlistners
+openModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
 
-// !Functions
-function addCard(event) {
-  // DIV Container --> The pop-up Card
-  const contDiv = document.createElement("div");
-  contDiv.classList.add("cont-div");
-  main.appendChild(contDiv);
+closeModalButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
 
-  //   DIV1 To fill Information
-  const data = document.createElement("div");
-  data.classList.add("data");
-  contDiv.appendChild(data);
+overlay.addEventListener("click", () => {
+  const modals = document.querySelectorAll(".modal.active");
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
 
-  // Title Form
-  const titleForm = document.createElement("form");
-  titleForm.classList.add("title");
-  data.appendChild(titleForm);
-  titleForm.setAttribute("onsubmit", "return false;"); //prevents the Form from submitting when hit enter after in input tag.
+// Functions
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
 
-  // DIV for input and icon
-  const inputIcon = document.createElement("div");
-  inputIcon.classList.add("input-icon");
-  titleForm.appendChild(inputIcon);
-
-  // Input
-  const input = document.createElement("input");
-  input.classList.add("title-input");
-  inputIcon.appendChild(input);
-  input.setAttribute("type", "text");
-  input.setAttribute("placeholder", "write Something");
-
-  // Icon
-  const titleIcon = document.createElement("i");
-  titleIcon.innerHTML = "<i class='fas fa-search'></i>";
-  titleIcon.classList.add("title-icon");
-  inputIcon.appendChild(titleIcon);
-
-  // Description
-  const description = document.createElement("textarea");
-  description.classList.add("data-description");
-  data.appendChild(description);
-
-  // DIV2 Seperator Line
-  const line = document.createElement("div");
-  line.classList.add("line");
-  contDiv.appendChild(line);
-
-  // DIV3 To Display Information
-  const dispData = document.createElement("div");
-  dispData.classList.add("display-data");
-  contDiv.appendChild(dispData);
-
-  // Cross Icon
-  const crossIcon = document.createElement("i");
-  crossIcon.innerHTML = "<i class='fas fa-times'></i>";
-  crossIcon.classList.add("cross-icon");
-  contDiv.appendChild(crossIcon);
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
 }
